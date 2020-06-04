446e6269038f7bccf8c1bd52752feda242863d62
ellacreid <59098975+ellacreid@users.noreply.github.com>
2020-06-04 00:15:49 -0700

    Tutorial 4.3.4 complete

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -31,4 +31,12 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log("Player collided with " + collision.gameObject + " with powerup set to " + hasPowerUp);
+        }
+    }
 }
